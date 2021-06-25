ea388875cd7b4b907fb718dd0625065c9c24c201
juliopalro <juliopalro@gmail.com>
2021-06-24 20:31:40 -0500

    Init project

---
+++ src/main/java/com/juliopalromarket/JuliopalroMarketApplication.java
@@ -0,0 +1,13 @@
+package com.juliopalromarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuliopalroMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuliopalroMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juliopalromarket/JuliopalroMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliopalromarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuliopalroMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
